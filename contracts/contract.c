7df15369d39c7708a060021807327ed20b65d656
AnupamRabha <anupam123rabha@gmail.com>
2025-04-12 18:08:19 +0530

    4th Commit

--- contracts/contract.c
+++ contracts/contract.c
@@ -1,8 +1,5 @@
 #include <stdint.h>
 int foo;
-int bar;
 void calc() {
-    foo = 100 - 30;
-    bar = foo;
-    foo = 20 + 10;
+    foo = 10 * 20;
 }
--- contracts/contract.sol
+++ contracts/contract.sol
@@ -1,12 +1,10 @@
-
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 contract MyContract {
     uint256 public foo;
-    uint256 public bar;
 
     function calc() public {
-        foo = 100 + 30;
-        bar = foo; 
-        foo = 20 - 10;
+        foo = 10 * 20;
     }
 }
